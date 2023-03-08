906848e84fe825abd12046d2629d8393abcdd652
HenBarper <Benjaminitonio@gmail.com>
2023-03-08 12:52:19 -0800

    Added char functionality to _printf.c

---
+++ _printf.c
@@ -0,0 +1,15 @@
+/**
+ * _printf - produce output according to format
+ * @format: the string to print
+ * Return: the number of chars printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	while (format[i] != '\0')
+	{
+		_putchar(format[i]);
+		i++;
+	}
+}
