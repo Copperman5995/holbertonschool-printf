cc1a81079de8c6ec0e50dd49474413f590c76c1d
Copperman5995 <cooper.d.mix@gmail.com>
2023-03-09 09:01:33 -0800

    added _strlen protoype

--- main.h
+++ main.h
@@ -15,6 +15,7 @@ typdef struct convert
 int _putchar(char c);
 void _puts(char *str);
 int _printf(const char *format, ...);
+int _strlen(char *str);
 
 // the difernt print functions//
 int print_string(va_list);
