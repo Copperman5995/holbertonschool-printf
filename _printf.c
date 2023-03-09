8c32294dcfa91f45a6f94eb24452317f51ca1cea
HenBarper <Benjaminitonio@gmail.com>
2023-03-09 10:25:53 -0800

    moved get convert func to _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,11 +1,37 @@
 #include "main.h"
 #include <stdarg.h>
 /**
+ * get_convert_func - looks for the letter
+ * @s: the thing that will be checked for the letter
+ * Return: the right function to be used
+ */
+
+int (*get_convert_func(const char *s))(va_list)
+{
+	convert_t converter[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	if(_strlen(s) != 1)
+		return (NULL);
+
+	while (converter[i].convert != NULL && converter[i].convert[0] != s[0])
+	{
+		i++;
+	}
+
+	return (converter[i].f);
+}
+/**
  * _printf - produce output according to format
  * @format: the string to print
  * Return: the number of chars printed
  */
-
 int _printf(const char *format, ...)
 {
 	int i = 0;
